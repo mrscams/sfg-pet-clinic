41e0c48ff28d87a97a8b4e5fa83c334b67ce52ac
mrscams <black.bug0@gmail.com>
2019-04-26 12:40:10 +0100

    add pets and contact info to owners on startup

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -26,6 +29,8 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+
+
       PetType dog = new PetType();
       dog.setName("dog");
       PetType savedDogType = petTypeService.save(dog);
@@ -33,6 +38,9 @@ public class DataLoader implements CommandLineRunner {
       PetType cat = new PetType();
       dog.setName("cat");
       PetType savedcatType = petTypeService.save(dog);
+
+
+
 
       Owner owner1 = new Owner();
       //owner1.setId(1L);
@@ -50,6 +58,22 @@ public class DataLoader implements CommandLineRunner {
       System.out.println("load Owners...");
 
       ownerService.save(owner1);
+
+      Pet mikesPet = new Pet();
+      mikesPet.setPetType(savedDogType);
+      mikesPet.setOwner(owner1);
+      mikesPet.setBirthDate(LocalDate.now());
+      mikesPet.setName("rock");
+
+     owner1.getPets().add(mikesPet);
+
+      Pet millouPet = new Pet();
+      millouPet.setPetType(savedDogType);
+      millouPet.setOwner(owner1);
+      millouPet.setBirthDate(LocalDate.now());
+      millouPet.setName("puppy");
+
+      owner2.getPets().add(millouPet);
 
       Vet vet1 = new Vet();
       //vet1.setId(1L);
